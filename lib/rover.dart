aec83dd2c0ae67e4b01fe0e2f5aa0a608b0e0803
pgloaguen <pgloaguen@mystudiofactory.com>
2021-04-19 10:45:41 +0200

    [DEV] Rover can move forward and backward

--- lib/rover.dart
+++ lib/rover.dart
@@ -1,9 +1,56 @@
 class Rover {
   Rover({required this.position, required this.orientation});
 
-  final Position position;
-  final Orientation orientation;
+  Position position;
+  Orientation orientation;
+
+  void move(Move move) {
+    switch (move) {
+      case Move.FORWARD:
+        _moveForward();
+        break;
+      case Move.BACKWARD:
+        _moveBackward();
+        break;
+    }
+  }
+
+  void _moveForward() {
+    switch (orientation) {
+      case Orientation.NORTH:
+        position = Position(x: position.x, y: position.y - 1);
+        break;
+      case Orientation.EAST:
+        position = Position(x: position.x + 1, y: position.y);
+        break;
+      case Orientation.SOUTH:
+        position = Position(x: position.x, y: position.y + 1);
+        break;
+      case Orientation.WEST:
+        position = Position(x: position.x - 1, y: position.y);
+        break;
+    }
+  }
+
+  void _moveBackward() {
+    switch (orientation) {
+      case Orientation.NORTH:
+        position = Position(x: position.x, y: position.y + 1);
+        break;
+      case Orientation.EAST:
+        position = Position(x: position.x - 1, y: position.y);
+        break;
+      case Orientation.SOUTH:
+        position = Position(x: position.x, y: position.y - 1);
+        break;
+      case Orientation.WEST:
+        position = Position(x: position.x + 1, y: position.y);
+        break;
+    }
+  }
 }
+
+enum Move { FORWARD, BACKWARD }
 
 enum Orientation { NORTH, EAST, SOUTH, WEST }
 
@@ -15,6 +62,9 @@ class Position {
   bool equals(Object position) {
     return position is Position && position.x == x && position.y == y;
   }
+
+  @override
+  String toString() => '($x, $y)';
 
   @override
   bool operator ==(Object other) =>
